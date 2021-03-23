99a82330ea181988b25490580b287b820dbaf2ba
sprout110 <sprout110@gmail.com>
2021-03-23 13:57:17 +0800

    ok

--- CoreAPI/Controllers/WeatherForecastController.cs
+++ CoreAPI/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace CoreAPI.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Testing"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
